a3d0ff4d9af4e6ac65d329d09c89e04b25509d19
huhn511 <sehe89@gmail.com>
2021-07-13 13:02:48 +0200

    setup docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,110 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'IOTA-2.0-Research-Specifications',
+  tagline: 'The research specifications of the IOTA 2.0 protocol.',
+  url: 'https://github.com/iotaledger/IOTA-2.0-Research-Specifications',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'iotaledger', // Usually your GitHub org/user name.
+  projectName: 'iota-v2', // Usually your repo name.
+  stylesheets: [
+    'https://fonts.googleapis.com/css?family=Material+Icons',
+  ],
+  themeConfig: {
+    navbar: {
+      logo: {
+        alt: 'IOTA-2.0-Research-Specifications',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'preface',
+          position: 'left',
+          label: 'Specifications',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/iotaledger/IOTA-2.0-Research-Specifications',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Documentation',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.iota.org',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/iota',
+            }, {
+              label: 'Stack Exchange',
+              href: 'https://iota.stackexchange.com/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/iotaledger/IOTA-2.0-Research-Specifications',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/iotaledger/IOTA-2.0-Research-Specifications/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/iotaledger/IOTA-2.0-Research-Specifications/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
