812d512bd8cb4103d4feddcb8cc03d7399ba5144
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-11 20:16:27 +0000

    Cleanup PR, set yarn version and update cli

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
 
 module.exports = {
-     title: 'IOTA 2.0 Research Specifications',
-     url: '/',
-     baseUrl: '/',
-     themes: ['@docusaurus/theme-classic'],
-     plugins: [
-         [
-             '@docusaurus/plugin-content-docs',
-             {
-                 id: 'IOTA-Research-Specifications',
-                 path: path.resolve(__dirname, '../'),
-                 routeBasePath: 'IOTA-2.0-Research-Specifications',
-                 sidebarPath: path.resolve(__dirname, '../sidebars.js'),
-                 editUrl: 'https://github.com/iotaledger/IOTA-2.0-Research-Specifications/edit/main',
-                 remarkPlugins: [require('remark-math'), require('rehype-katex')],
-                 exclude: [
+    title: 'IOTA 2.0 Research Specifications',
+    url: '/',
+    baseUrl: '/',
+    themes: ['@docusaurus/theme-classic'],
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'IOTA-Research-Specifications',
+                path: path.resolve(__dirname, '../'),
+                routeBasePath: 'IOTA-2.0-Research-Specifications',
+                sidebarPath: path.resolve(__dirname, '../sidebars.js'),
+                editUrl: 'https://github.com/iotaledger/IOTA-2.0-Research-Specifications/edit/main',
+                remarkPlugins: [require('remark-math'), require('rehype-katex')],
+                exclude: [
                     '**/docusaurus',
-                  ],
-             }
-         ],
-     ],
-     staticDirectories: [],
- };
+                ],
+            }
+        ],
+    ],
+    staticDirectories: [],
+};
